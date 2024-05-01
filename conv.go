2ef1b6ccd900a5e144894fe9ed20c46496ec67de
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-05-01 20:04:39 +0500

    Added all converter functions

--- conv.go
+++ conv.go
@@ -5,12 +5,27 @@ func CelToFar(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9/5 + 32)
 }
 
+// CellToKel преобразует температуру по Цельсию в температуру по Кельвину
+func CellToKel(c Celsius) Kelvin {
+	return Kelvin(c + 273.15)
+}
+
 // FahToCel преобразует температуру по Фаренгейту в температуру по Цельсию
 func FahToCel(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
 }
 
-// KToC преобразует температуру по Кельвину в температуру по Цельсию
+// FahToKel преобразует температуру по Фаренгейту в температуру по Кельвину
+func FahToKel(f Fahrenheit) Kelvin {
+	return Kelvin((f-32)*5/9 + 273.15)
+}
+
+// KelToCel преобразует температуру по Кельвину в температуру по Цельсию
 func KelToCel(k Kelvin) Celsius {
 	return Celsius(k - 273.15)
+}
+
+// KelToFar преобразует температуру по Кельвину в температуру по Фаренгейту
+func KelToFar(k Kelvin) Fahrenheit {
+	return Fahrenheit((k-273.15)*9/5 + 32)
 }
