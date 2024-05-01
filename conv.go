4e97009fc398218f1e3a74db2d9a02b0322efc20
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-05-01 17:39:35 +0500

    refactor: improve readability

--- conv.go
+++ conv.go
@@ -1,16 +1,16 @@
 package tempconv
 
-// CToF преобразует температуру по Цельсию в температуру по Фаренгейту.
-func CToF(c Celsius) Fahrenheit {
+// CelToFar преобразует температуру по Цельсию в температуру по Фаренгейту.
+func CelToFar(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9/5 + 32)
 }
 
-// FToC преобразует температуру по Фаренгейту в температуру по Цельсию
-func FToC(f Fahrenheit) Celsius {
+// FahToCel преобразует температуру по Фаренгейту в температуру по Цельсию
+func FahToCel(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
 }
 
 // KToC преобразует температуру по Кельвину в температуру по Цельсию
-func KToC(k Kelvin) Celsius {
+func KelToCel(k Kelvin) Celsius {
 	return Celsius(k - 273.15)
 }
--- tempconv_test.go
+++ tempconv_test.go
@@ -3,20 +3,42 @@
 package tempconv
 
 import (
+	"fmt"
 	"github.com/unixlinuxgeek/floatgen" // Используем вспомогательную библиотеку для генерации чисел с плав. запятой
+	"os"
 	"testing"
 )
 
-func TestKelvinToC(t *testing.T) {
-	rnd := floatgen.GenRan(1, 9)
+// Checking  Kelvin to Celsius (KelToC) converting
+func TestKelToC(t *testing.T) {
+	rnd := getFltRnd()
 	o := rnd - 273.15
 
 	kc := KToC(Kelvin(rnd))
 	c := Celsius(o)
 
 	if kc == c {
-		t.Logf("Test TestKelvinToC is Passed!!!  %f °K == %f °C", rnd, o)
+		t.Logf("Test TestKelvinToC is Passed!!!  %.2f °K == %.2f °C", rnd, o)
 	} else {
 		t.Fatal("Test TestKelvinToC Not Passed")
 	}
+}
+
+// Checking Celsius to Fahrenheit (CToF) converting
+func TestCToFar(t *testing.T) {
+	rnd := getFltRnd()
+
+	f := Fahrenheit((rnd * 9 / 5) + 32)
+
+	cf := CToF(Celsius(rnd))
+	if cf == f {
+		fmt.Fprintf(os.Stdout, "Test Passed!!! %.2f °C equal %.2f °F", rnd, f)
+	} else {
+		fmt.Fprintf(os.Stdout, "Test Failed: %.2f °C not equal %f.2 °F", rnd, f)
+	}
+}
+
+// Generation random value from 1 to 9
+func getFltRnd() float64 {
+	return floatgen.GenRan(1, 9)
 }
