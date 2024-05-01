842f776012ca4d770744a719e987ab7d6885c5eb
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-05-02 00:32:10 +0500

    Added Test Functions

--- tempconv_test.go
+++ tempconv_test.go
@@ -24,8 +24,24 @@ func TestKelToCel(t *testing.T) {
 	}
 }
 
+// Kel -> Fah
+// (32K − 273.15) × 9/5 + 32 = -402.1°F
+func TestKelToFah(t *testing.T) {
+	rnd := getFltRnd()
+	o := (rnd-273.15)*9/5 + 32
+
+	conv := KelToFah(Kelvin(rnd))
+	f := Fahrenheit(o)
+
+	if f == conv {
+		t.Logf("Test TestKelToFah is Passed!!!  %.2f °K == %.2f °F\n", rnd, conv)
+	} else {
+		t.Fatalf("Test Failed %.2f °K  not equal %.2f °F \n", f, conv)
+	}
+}
+
 // Checking Celsius to Fahrenheit (CelToFah) converting
-func TestCelToFar(t *testing.T) {
+func TestCelToFah(t *testing.T) {
 	rnd := getFltRnd()
 
 	f := Fahrenheit((rnd * 9 / 5) + 32)
@@ -34,7 +50,19 @@ func TestCelToFar(t *testing.T) {
 	if cf == f {
 		fmt.Fprintf(os.Stdout, "Test Passed!!! %.2f °C equal %.2f °F", rnd, f)
 	} else {
-		fmt.Fprintf(os.Stdout, "Test Failed: %.2f °C not equal %f.2 °F", rnd, f)
+		t.Fatalf("Test Failed: %.2f °C not equal %f.2 °F", rnd, f)
+	}
+}
+
+// Checking Celsius to Kelvin (CelToKel) converting
+func TestCelToKel(t *testing.T) {
+	rnd := getFltRnd()
+	k := Kelvin((rnd-273.15)*9/5 + 32)
+	ck := CelToKel(Celsius(rnd))
+	if k == ck {
+		fmt.Fprintf(os.Stdout, "Test Passed!!! %.2f °C equal %.2f °K", rnd, k)
+	} else {
+		t.Fatalf("Test Failed: %.2f °C not equal %f.2 °K", rnd, k)
 	}
 }
 
@@ -44,12 +72,27 @@ func TestFahToCel(t *testing.T) {
 	f := Fahrenheit(rnd)
 
 	// converting Fahrenheit to Celsius formula
-	fToc := Celsius((f - 32) * 5 / 9)
+	fToC := Celsius((f - 32) * 5 / 9)
 
-	if fToc == FahToCel(Fahrenheit(rnd)) {
-		fmt.Fprintf(os.Stdout, "Test Passed!!! %.2f °F equal %.2f °C", rnd, fToc)
+	if fToC == FahToCel(Fahrenheit(rnd)) {
+		fmt.Fprintf(os.Stdout, "Test Passed!!! %.2f °F equal %.2f °C", rnd, fToC)
 	} else {
-		fmt.Fprintf(os.Stdout, "Test Failed: %.2f °F not equal %.2f.2 °C", rnd, fToc)
+		t.Fatalf("Test Failed: %.2f °F not equal %.2f.2 °C", rnd, fToC)
+	}
+}
+
+// (0°F − 32) × 5/9 + 273.15 = 255.372K
+// Far -> Kel
+func TestFahToKel(t *testing.T) {
+	rnd := getFltRnd()
+
+	//  far to kel formula
+	k := Kelvin((rnd-32)*5/9 + 273.15)
+
+	if k == FahToKel(Fahrenheit(rnd)) {
+
+	} else {
+		t.Fatalf("Test Failed: %.2f °F not equal %.2f °K", rnd, k)
 	}
 }
 
