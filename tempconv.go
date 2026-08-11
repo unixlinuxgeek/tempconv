fdf3d745c5108de0e5f6943662f5dd437941afdd
aspieunixgeek <aspie.c.coder@gmail.com>
2026-08-12 00:38:25 +0500

    Initial commit

---
+++ tempconv.go
@@ -0,0 +1,21 @@
+// Package tempconv performs Celsius and Fahrenheit conversations.
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.13
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g°F")
+}
